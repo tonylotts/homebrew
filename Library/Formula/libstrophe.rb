3f895248427bb15762e188a2fa44c0827137abd3
Tony Lotts <tony.lotts@gmail.com>
2014-02-25 13:00:28 -0500

    Clean up libstrophe formula * Adds libxml2 option

--- Library/Formula/libstrophe.rb
+++ Library/Formula/libstrophe.rb
@@ -3,18 +3,29 @@ require "formula"
 class Libstrophe < Formula
   homepage "http://strophe.im/libstrophe"
   url "https://github.com/strophe/libstrophe.git"
-  #sha1 ""
+  sha1 "d408eaf2bbfe5ff5c56eab01463c278f9891c08e"
   version "current"
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "expat"
+  option "with-libxml2", "Use libxml2 (default expat)"
+
+  depends_on :autoconf
+  depends_on :automake
+  depends_on "libxml2" => :optional
 
   def install
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --mandir=#{man}
+    ]
+    if build.with? "libxml2"
+      args.push(--with-libxml2)
+    end
+
+    man.mkpath
     system "./bootstrap.sh"
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--mandir=#{man}"
+    system "./configure", *args
     system "make install"
   end
 end
